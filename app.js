c64f55276bdc0b061c36d1c9a98df97b57b8ffb2
Nigel Sprague <spraguenigel27@gmail.com>
2024-08-23 11:10:07 -0600

    updates and draws perClick, perAuto, and resourceBank info on load in and collection

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 console.log('wazzap')
+
+let resource = 0
+let perClick = 1
+let perAuto = 0
+
+let clickUpgrades = [
+  {
+    name: 'pickaxe',
+    price: 100,
+    quantity: 0,
+    bonus: 1
+  }
+];
+
+let automaticUpgrades = [
+  {
+    name: 'rover',
+    price: 600,
+    quantity: 0,
+    bonus: 20
+  }
+];
+
+function collectResource() {
+  resource++
+  updateResource()
+}
+
+function updateResource() {
+  let click = document.getElementById('perClick')
+  let auto = document.getElementById('perAuto')
+  let bank = document.getElementById('resourceBank')
+  click.innerText = perClick
+  auto.innerText = perAuto
+  bank.innerText = resource
+}
+updateResource()
