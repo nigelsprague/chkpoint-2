2fe49187c7e6bda11b0e2d1201152f10724567ee
Nigel Sprague <spraguenigel27@gmail.com>
2024-08-25 22:13:16 -0600

    styled

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ let automaticUpgrades = [
     name: 'asmall',
     price: 1200,
     quantity: 0,
-    bonus: 20
+    bonus: 10
   },
   {
     name: 'abig',
@@ -57,7 +57,7 @@ function updateResource() {
   let bank = document.getElementById('resourceBank')
   let total = document.getElementById('total')
   bank.innerText = resource
-  total.innerText = resourceOverTime
+  total.innerText = 'Total Criterions Over Time: ' + resourceOverTime
 }
 updateResource()
 
@@ -71,49 +71,64 @@ function updateUpgrades() {
   let aSmall = automaticUpgrades[0]
   let aBig = automaticUpgrades[1]
 
-  click.innerText = perClick
-  auto.innerText = perAuto
+  click.innerText = '+' + perClick
+  auto.innerText = '+' + perAuto + '/sec'
   stats.innerHTML = `
-    <section class="row">
-      <div class="col-6">
-      <h6>Click Stats</h6>
-        <section class="row">
-          <div class="col-2 card">${cSmall.quantity}</div>
-          <div class="col-6">=></div>
-          <div class="col-2 card">${cSmall.quantity * cSmall.bonus}</div>
-        </section>
-        <section class="row">
-          <div class="col-2 card">${cBig.quantity}</div>
-          <div class="col-6">=></div>
-          <div class="col-2 card">${cBig.quantity * cBig.bonus}</div>
-        </section>
-      </div>
-      <div class="col-6">
-      <h6>Automatic Stats</h6>
-        <section class="row">
-          <div class="col-2 card">${aSmall.quantity}</div>
-          <div class="col-6">=></div>
-          <div class="col-2 card">${aSmall.quantity * aSmall.bonus}</div>
-        </section>
-        <section class="row">
-          <div class="col-2 card">${aBig.quantity}</div>
-          <div class="col-6">=></div>
-          <div class="col-2 card">${aBig.quantity * aBig.bonus}</div>
-        </section>
-      </div>
-    </section>`
+    <section class="row text-center justify-content-between p-2">
+          <div class="col-5 me-3">
+            <h6 class="bungee-regular text-center">Click Stats</h6>
+            <section class="row justify-content-between my-3">
+              <div class="col-3 px-1 border bungee-regular">${cSmall.quantity}</div>
+              <div class="col-6 px-2">Dbl Feature =></div>
+              <div class="col-3 border px-1 bungee-regular">${cSmall.quantity * cSmall.bonus}</div>
+            </section>
+            <section class="row justify-content-between">
+              <div class="col-3 px-1 border bungee-regular">${cBig.quantity}</div>
+              <div class="col-6 px-2">Box Set =></div>
+              <div class="col-3 px-1 border bungee-regular">${cBig.quantity * cBig.bonus}</div>
+            </section>
+          </div>
+          <div class="col-6">
+            <h6 class="bungee-regular text-center">Automatic Stats</h6>
+            <section class="row justify-content-between my-3">
+              <div class="col-3 px-1 border bungee-regular">${aSmall.quantity}</div>
+              <div class="col-6 px-2">Subscription =></div>
+              <div class="col-3 px-1 border bungee-regular">${aSmall.quantity * aSmall.bonus}</div>
+            </section>
+            <section class="row justify-content-between">
+              <div class="col-3 px-1 border bungee-regular">${aBig.quantity}</div>
+              <div class="col-6 px-2">Closet =></div>
+              <div class="col-3 px-1 border bungee-regular">${aBig.quantity * aBig.bonus}</div>
+            </section>
+          </div>
+        </section>`
 
   upgrades.innerHTML = `
     <div class="col-6">
-      <h6>Click Upgrades</h6>
-      <button onclick="buyUpgrade('clickUpgrades', 'csmall')" class="btn bg-info">${cSmall.price}</button>
-      <button onclick="buyUpgrade('clickUpgrades', 'cbig')" class="btn bg-info">${cBig.price}</button>
-    </div>
-    <div class="col-6">
-      <h6>Automatic Upgrades</h6>
-      <button onclick="buyUpgrade('automaticUpgrades', 'asmall')" class="btn bg-info">${aSmall.price}</button>
-      <button onclick="buyUpgrade('automaticUpgrades', 'abig')" class="btn bg-info">${aBig.price}</button>
-    </div>`
+            <h6 class="bungee-regular text-center">Click Upgrades</h6>
+            <section class="row my-3">
+              <button onclick="buyUpgrade('clickUpgrades', 'csmall')" class="col-4 bungee-regular btn">$${cSmall.price}</button><span
+                class="col pe-0">Double Feature +1</span>
+            </section>
+            <section class="row mb-3">
+              <button onclick="buyUpgrade('clickUpgrades', 'cbig')" class="col-4 bungee-regular btn">$${cBig.price}</button><span
+                class="col pe-0">Director's
+                Box-Set +5</span>
+            </section>
+          </div>
+          <div class="col-6">
+            <h6 class="bungee-regular text-center">Automatic Upgrades</h6>
+            <section class="row my-3">
+              <button onclick="buyUpgrade('automaticUpgrades', 'asmall')" class="col-4 bungee-regular btn">$${aSmall.price}</button>
+              <span class="col pe-0">Criterion Subscription +100 / 3s</span>
+            </section>
+            <section class="row mb-3">
+              <button onclick="buyUpgrade('automaticUpgrades', 'abig')" class="col-4 bungee-regular btn">$${aBig.price}</button>
+              <span class="col pe-0">Criterion Closet +1000 / 3s</span>
+            </section>
+          </div>
+        </section>
+      </div>`
 }
 updateUpgrades()
 
@@ -128,7 +143,7 @@ function buyUpgrade(upgradeType, upgradeName) {
         upgrade.price += (upgrade.price * 1 + 1)
         // console.log('purchased', upgradeName, clickUpgrades)
 
-      } else console.log('no')
+      } else alert('You dont have enough cheddah, get yo money up Big Dawg')
     })
   } else if (upgradeType == 'automaticUpgrades') {
     let upgrade = automaticUpgrades.filter(upgrade => upgrade.name == upgradeName)
@@ -139,7 +154,7 @@ function buyUpgrade(upgradeType, upgradeName) {
         upgrade.price += (upgrade.price * 1 + 3)
         automaticCollection = true
         // console.log('purchased', upgradeName, automaticUpgrades)
-      } else console.log('no')
+      } else alert('You dont have enough cheddah, get yo money up Big Dawg')
     })
   }
   collectRate()
